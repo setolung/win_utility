c29c95ee1d20f4ef39b127220dd5583d4e17d734
setolung <65989991+setolung@users.noreply.github.com>
2023-11-28 08:12:35 +0000

    Add files via upload

---
+++ writeSpace.ps1
@@ -0,0 +1,12 @@
+﻿param($minutes = 60)
+
+$myShell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $minutes; $i++) {
+  Start-Sleep -Seconds 61
+ # $myShell.sendkeys(" ")
+  $myShell.sendkeys("$(get-date -f HH_mm_ss) $i")
+} 
+
+$myShell.sendkeys("$minutes passed")
+
